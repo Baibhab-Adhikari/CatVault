a9cdce97f6c8d388a44c46334e87418ef161511c
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-11-11 12:57:15 +0530

    added routes for login and register

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,34 @@
 from flask import render_template, redirect, url_for, request, flash, session
-from app import app 
+from app import app
 
+
+# homepage route
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+# registration route
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    
+    # for GET:
+    if request.method == 'GET':
+        return render_template("register.html")
+    
+    elif request.method == "POST":
+        pass
+
+
+# login route
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    
+    # for GET 
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    
