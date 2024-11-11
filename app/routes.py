82d4a94c9f287df4fb0b30f54e7899a81448e9ec
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-11-11 18:37:19 +0530

    login, 404, some routes are handled

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,18 @@ from flask import render_template, redirect, url_for, request, flash, session
 from app import app
 
 
+# error handler route (suggested by chatgpt)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 400
+
+
 # homepage route
 
 @app.route("/")
 def index():
+    """route for the homepage"""
+
     return render_template("index.html")
 
 # registration route
@@ -13,22 +21,29 @@ def index():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    
+    """Registration for new user"""
     # for GET:
     if request.method == 'GET':
         return render_template("register.html")
-    
+
+    # for POST:
     elif request.method == "POST":
-        pass
+
+        # variables to store info
+        first_name = request.form.get("firstname")
+        last_name = request.form.get("lastname")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirmpassword")
+
+    return redirect("/")
 
 
 # login route
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    
-    # for GET 
+
+    # for GET
     if request.method == "GET":
         return render_template("login.html")
-    
-    
