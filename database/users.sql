15fef68cb2b64059393e81e6ad118d077909516d
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-11-13 22:34:30 +0530

    Stop tracking the database folder and update .gitignore

--- database/users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    hashed_password TEXT NOT NULL
-)
