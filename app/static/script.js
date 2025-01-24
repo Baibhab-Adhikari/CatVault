105b6744d951a2f08954dddedcb37d39a8d86103
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2025-01-24 23:32:54 +0530

    Implement flash message display and remove unused theme toggle functionality

--- app/static/script.js
+++ app/static/script.js
@@ -1,60 +1,10 @@
-// code adapted from github copilot - dark and light mode functionality (I am not comfortable with JS, sorry!)
-
-document.addEventListener("DOMContentLoaded", function () {
-  const themeToggle = document.getElementById("theme-toggle");
-
-  // Load the user's theme preference from localStorage
-  const userTheme = localStorage.getItem("theme");
-
-  if (userTheme === "dark") {
-    document.body.classList.add("dark-mode");
-    document.body.classList.remove("light-mode");
-    themeToggle.checked = true;
-  } else if (userTheme === "light") {
-    document.body.classList.add("light-mode");
-    document.body.classList.remove("dark-mode");
-    themeToggle.checked = false;
-  } else {
-    // Default to dark mode if no preference is set
-    document.body.classList.add("dark-mode");
-    themeToggle.checked = true;
-  }
-
-  themeToggle.addEventListener("change", function () {
-    if (this.checked) {
-      document.body.classList.add("dark-mode");
-      document.body.classList.remove("light-mode");
-      localStorage.setItem("theme", "dark");
-      document.querySelectorAll("a").forEach((link) => {
-        link.classList.add("dark-mode");
-        link.classList.remove("light-mode");
-      });
-      document.querySelectorAll(".dropdown-menu").forEach((burger) => {
-        burger.classList.add("dark-mode");
-        burger.classList.remove("light-mode");
-      });
-    } else {
-      document.body.classList.add("light-mode");
-      document.body.classList.remove("dark-mode");
-      localStorage.setItem("theme", "light");
-      document.querySelectorAll("a").forEach((link) => {
-        link.classList.add("light-mode");
-        link.classList.remove("dark-mode");
-      });
-      document.querySelectorAll(".dropdown-menu").forEach((burger) => {
-        burger.classList.add("light-mode");
-        burger.classList.remove("dark-mode");
-      });
-    }
-  });
-  // Hide flash messages after 2.5 seconds
+ // Hide flash messages after 2.5 seconds
   const flashMessages = document.getElementById("flash-messages");
   if (flashMessages) {
     setTimeout(() => {
       flashMessages.style.display = "none";
     }, 2500);
   }
-});
 
 // js code for the range slider
 
